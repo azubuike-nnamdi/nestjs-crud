4ac50cfcc16ef8bad90c27c0214b32865103062d
Nnamdi Azubuike <127966002+nnamdi-azubuike@users.noreply.github.com>
2024-06-12 09:51:17 +0100

    chore: user module :green_heart:

---
+++ src/users/users.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './controllers/users/users.controller';
+
+//tells nest that this is a module
+@Module({
+  controllers: [UsersController],
+})
+export class UsersModule { }
